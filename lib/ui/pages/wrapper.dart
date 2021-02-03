c99a60336e704b2ba4f4fc472f32b529950de535
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2021-02-03 23:58:08 +0700

    feat: connect flutter to fcm

--- lib/ui/pages/wrapper.dart
+++ lib/ui/pages/wrapper.dart
@@ -1,15 +1,90 @@
 part of 'pages.dart';
 
-class Wrapper extends StatelessWidget {
+class Wrapper extends StatefulWidget {
+  @override
+  _WrapperState createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  String messageTitle = "Empty";
+  String notificationAlert = "alert";
+
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  String token;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // hit api cek access token yang lama
+    setToken();
+    // bandingin
+    // kalo beda hit beda api ke backend
+
+    _firebaseMessaging.configure(
+      onMessage: (message) async {
+        print("onMessage: $message");
+
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = token;
+        });
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        setState(() {
+          messageTitle = message["notification"]["title"];
+          notificationAlert = "New Notification Alert";
+        });
+      },
+      onResume: (message) async {
+        print("onResume: $message");
+        setState(() {
+          messageTitle = message["data"]["title"];
+          notificationAlert = "Application opened from Notification";
+        });
+      },
+    );
+  }
+
+  void setToken() async {
+    token = await _firebaseMessaging.getToken();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: nanti disini bakal ada if else dari bloc builder yang bakal ke re render ketika menerima page state berbeda
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Test"),
+      appBar: AppBar(
+        title: Text("test"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              token,
+            ),
+            Text(
+              messageTitle,
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+
+  // Or do other work.
 }
