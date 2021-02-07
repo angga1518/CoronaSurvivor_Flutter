0bede1f4448321b78bff7084cd4cabe9c839c8eb
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2021-02-07 20:54:38 +0700

    feat: add ex

--- lib/ui/pages/splash_page.dart
+++ lib/ui/pages/splash_page.dart
@@ -12,7 +12,8 @@ class _SplashPageState extends State<SplashPage> {
     UserBloc userBloc = BlocProvider.of<UserBloc>(context);
     return Scaffold(
       body: Container(
-        height: UIHelper.responsive.setWidth(150),
+        color: UIHelper.color1,
+        height: UIHelper.responsive.setHeight(160.0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -23,12 +24,9 @@ class _SplashPageState extends State<SplashPage> {
                   await AuthServices.signUp("erlanggamuhammad01@gmail.com", "corona", "Angga");
                 },
               ),
-              RaisedButton(
-                child: Text("SignIn", style: UIHelper.testFont.copyWith(fontSize: 15),),
-                onPressed: () async {
-                  await AuthServices.signIn("erlanggamuhammad01@gmail.com", "corona");
-                },
-              ),
+              ButtonUniversal(50, ()async{
+                await AuthServices.signIn("erlanggamuhammad01@gmail.com", "corona");
+              })
             ],
           ),
         ),
---
+++ lib/ui/widgets/button.dart
@@ -0,0 +1,22 @@
+part of 'widgets.dart';
+
+class ButtonUniversal extends StatelessWidget {
+
+  final int height;
+  final Function onTap;
+  ButtonUniversal(this.height, this.onTap);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text(
+          "SignIn",
+          style: UIHelper.testFont.copyWith(fontSize: 15),
+        ),
+        onPressed: () async {
+        },
+      ),
+    );
+  }
+}
